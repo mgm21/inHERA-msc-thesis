6c4ba5dcd52509816a07e7a43083a9c503eaf458
Matteo Mecattaf <72270231+mgm21@users.noreply.github.com>
2023-07-27 14:34:01 +0100

    Added flag to remove BDs with a -inf fitness  when loading repertoire

--- src/repertoire_loader.py
+++ src/repertoire_loader.py
@@ -1,16 +1,26 @@
 from src.all_imports import *
 
+# TODO: check that it is okay to remove the empty bds as shown below
 class RepertoireLoader:
-    def load_repertoire(self, repertoire_path="./last_repertoire/"):
+    def load_repertoire(self, repertoire_path="./results/last_repertoire/", remove_empty_bds=True):
         centroids = jnp.load(os.path.join(repertoire_path, "centroids.npy"))
         descriptors = jnp.load(os.path.join(repertoire_path, "descriptors.npy"))
         fitnesses = jnp.load(os.path.join(repertoire_path, "fitnesses.npy"))
         genotypes = jnp.load(os.path.join(repertoire_path, "genotypes.npy"))
+
+        if remove_empty_bds:
+            centroids = centroids[fitnesses != -jnp.inf]
+            descriptors = descriptors[fitnesses != -jnp.inf]
+            genotypes = genotypes[fitnesses != -jnp.inf]
+            # NB important to keep fitnesses refactor for the end because affects the ones above
+            fitnesses = fitnesses[fitnesses != -jnp.inf]
+
+            
         return centroids, descriptors, fitnesses, genotypes
 
 if __name__ == "__main__":
     repertoire_loader = RepertoireLoader()
     
-    rep_arrs_1 = repertoire_loader.load_repertoire(repertoire_path="./last_repertoire/")
-    rep_arrs_2 = repertoire_loader.load_repertoire(repertoire_path="./class-last_repertoire/")
-    rep_arrs_3 = repertoire_loader.load_repertoire(repertoire_path="./class_example_repertoire/")    
+    rep_arrs_1 = repertoire_loader.load_repertoire(repertoire_path="./results//last_repertoire/")
+    rep_arrs_2 = repertoire_loader.load_repertoire(repertoire_path="./results//class-last_repertoire/")
+    rep_arrs_3 = repertoire_loader.load_repertoire(repertoire_path="./results//class_example_repertoire/")    
