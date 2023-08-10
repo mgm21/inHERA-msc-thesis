e7e3af04a6cf72b23c50d99197d0eaa866a781fc
Matteo Mecattaf <72270231+mgm21@users.noreply.github.com>
2023-08-10 17:59:20 +0100

    Implemented methods to save and reset the family's ancestor arrays

--- src/core/family.py
+++ src/core/family.py
@@ -9,11 +9,31 @@ class Family:
         rep_arrs = rep_load.load_repertoire(repertoire_path=f"{path_to_family}/repertoire")
         self.centroids, self.descriptors, self.sim_fitnesses, self.genotypes = rep_arrs
 
-        # Define the family ancestors' mu and var
+        # Define the ancestor arrays
         ancestors_loader = AncestorsLoader()
         self.ancestor_mus, self.ancestors_vars, self.ancestors_names, self.damage_dicts = ancestors_loader.load_ancestors(path_to_ancestors=f"{path_to_family}/ancestors")
 
-        # Define the family ancestor's 
+        # Save a copy of the original ancestor arrays
+        self._save_copy_of_init_ancest_arrs()
+
+    # TODO: all of these may not be needed but just in case
+    def _save_copy_of_init_ancest_arrs(self,):
+        self.init_ancestor_mus = jnp.copy(self.ancestor_mus)
+        self.init_ancestor_vars = jnp.copy(self.ancestors_vars)
+        self.init_ancestor_names = jnp.copy(self.ancestors_names)
+        self.init_damage_dicts = jnp.copy(self.damage_dicts)
+    
+    def reset_the_ancestor_arrs(self,):
+        # Delete the previous ones from memory to make sure they do not occupy too much memory
+        del self.ancestor_mus
+        del self.ancestors_vars
+        del self.ancestors_names
+        del self.damage_dicts
+
+        self.ancestor_mus = jnp.copy(self.init_ancestor_mus)
+        self.ancestors_vars = jnp.copy(self.init_ancestor_vars)
+        self.ancestors_names = jnp.copy(self.init_ancestor_names)
+        self.damage_dicts = jnp.copy(self.init_damage_dicts)
 
 if __name__ == "__main__":
     fam = Family(path_to_family="results/family_4_1")
