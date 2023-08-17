65df6c78a7894a1833565fc5530792d2a298b0c1
Matteo Mecattaf <72270231+mgm21@users.noreply.github.com>
2023-08-17 14:27:11 +0100

    Changed the constraint bounds of GPCFReg

--- src/adaptation_algorithms/gpcf_reg.py
+++ src/adaptation_algorithms/gpcf_reg.py
@@ -11,7 +11,7 @@ class GPCFReg(GPCFVariant):
         
         # Details of regularisation
         self.gaussian_process.set_box_gradient_projection()
-        self.gaussian_process.set_projection_hyperparameters(hyperparams=(0, 1))
+        self.gaussian_process.set_projection_hyperparameters(hyperparams=(-jnp.inf, +jnp.inf))
         self.gaussian_process.loss = self.gaussian_process.loss_regularised_l1_invmax
     
     def get_ancestor_weights(self, counter):
